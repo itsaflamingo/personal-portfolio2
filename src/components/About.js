df4eb5a9a1f5f728962ea341e87c535ca08ac735
Elizabeth Pinero <elizabeth.pinero.r@gmail.com>
2024-11-07 22:10:01 -0500

    styled about-me

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,11 @@ import me from "../images/me.jpeg";
 
 export default function About() {
   return (
-    <div className="flex">
-      <div>
+    <div className="flex flex-col justify-center w-3/5">
+      <div className="flex justify-center">
         <h3>Who am I?</h3>
+      </div>
+      <div className="flex">
         <p>
           A <span>full-stack developer</span> who took the long way around. I
           graduated in 2020 with a degree in International Development. During
@@ -13,8 +15,8 @@ export default function About() {
           myself web development through The Odin Project, where I created over
           30 projects of various complexities.{" "}
         </p>
+        <img alt="me" src={`${me}`} className="w-1/5"></img>
       </div>
-      <img alt="me" src={`${me}`} className="w-1/5"></img>
     </div>
   );
 }
